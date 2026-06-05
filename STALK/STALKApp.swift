c96b7c988e28617260aeaa46f0a97165229c5c0d
ITAMARAZI <itamar.bar.zohar@gmail.com>
2026-06-05 16:27:36 +0300

    Initial Commit

---
+++ STALK/STALKApp.swift
@@ -0,0 +1,17 @@
+//
+//  STALKApp.swift
+//  STALK
+//
+//  Created by itamar bar zohar on 05/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct STALKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
